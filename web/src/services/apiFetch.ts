8a42cee64104a990359beb79374a070861b9cd04
jprkindrid <jpriveraa7@gmail.com>
2025-10-16 19:27:07 -0500

    dev: refactor apiFetch to prepare for upload form

--- web/src/services/apiFetch.ts
+++ web/src/services/apiFetch.ts
@@ -3,18 +3,22 @@ import { API_URL } from "../utils/constants";
 type RestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 export async function apiFetch(
-  method: RestMethod,
-  path: string,
-  options: RequestInit = {}
+    method: RestMethod,
+    path: string,
+    options: RequestInit = {}
 ): Promise<Response> {
-  const res = await fetch(`${API_URL}${path}`, {
-    method: method,
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      ...options.headers,
-    },
-    ...options,
-  });
-  return res;
+    const isFormData = options.body instanceof FormData;
+
+    const headers: HeadersInit = {
+        ...(isFormData ? {} : { "Content-Type": "application/json" }),
+        ...options.headers,
+    };
+
+    const res = await fetch(`${API_URL}${path}`, {
+        method: method,
+        credentials: "include",
+        headers,
+        ...options,
+    });
+    return res;
 }
