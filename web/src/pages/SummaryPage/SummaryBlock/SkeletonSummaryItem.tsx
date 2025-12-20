47bec291ff23915b47fbfc61e1f6cf07474c3b34
jprkindrid <jpriveraa7@gmail.com>
2025-12-19 20:43:54 -0600

    fix: fixed skeleton item sizing to match actual items

--- web/src/pages/SummaryPage/SummaryBlock/SkeletonSummaryItem.tsx
+++ web/src/pages/SummaryPage/SummaryBlock/SkeletonSummaryItem.tsx
@@ -1,22 +1,22 @@
 const SkeletonSummaryItem = () => {
     return (
-        <div className="flex w-full animate-pulse items-center px-6 py-2">
-            <div className="mr-4 h-20 w-20 rounded bg-stone-300 dark:bg-stone-700"></div>
+        <div className="mt-2 flex w-full animate-pulse items-center text-xl sm:text-2xl">
+            <div className="mx-6 flex w-full items-center px-4 py-2">
+                <div className="dark:ring-spotify-green/50 mr-4 h-20 w-20 shrink-0 overflow-hidden rounded shadow-lg dark:ring"></div>
 
-            <div className="flex w-full flex-1 items-center justify-between sm:flex-row">
-                <div className="flex flex-1 flex-col sm:flex-row">
-                    <div className="flex items-center gap-2 sm:w-1/2">
-                        <div className="h-6 w-6 rounded bg-stone-300 dark:bg-stone-700"></div>
-                        <div className="h-6 w-2/3 rounded bg-stone-300 dark:bg-stone-700"></div>
+                <div className="block flex-1 flex-col items-center sm:flex sm:flex-row">
+                    <div className="flex sm:w-1/2">
+                        <div className="mr-2 flex font-bold">
+                            <div className="h-6 w-8 rounded bg-neutral-300 dark:bg-neutral-700"></div>
+                        </div>
+                        <div className="h-6 w-2/3 rounded bg-neutral-300 dark:bg-neutral-700"></div>
                     </div>
 
-                    <div className="mt-1 flex flex-1 gap-1 text-base sm:mt-0 sm:flex-row">
-                        <div className="h-4 w-1/3 rounded bg-stone-300 dark:bg-stone-700"></div>
-                        <div className="h-4 w-1/4 rounded bg-stone-300 dark:bg-stone-700"></div>
+                    <div className="flex flex-1 flex-col text-sm tabular-nums sm:flex-row sm:text-base">
+                        <div className="mr-1 h-4 w-1/3 rounded bg-neutral-300 dark:bg-neutral-700"></div>
+                        <div className="h-4 w-1/4 rounded bg-neutral-300 dark:bg-neutral-700"></div>
                     </div>
                 </div>
-
-                <div className="h-12 w-12 rounded-full bg-stone-300 px-4 dark:bg-stone-700"></div>
             </div>
         </div>
     );
